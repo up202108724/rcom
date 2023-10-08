3793e3f3445afa967cf81ab80ddc58354819cf9b
up202108724 <up202108724@g.uporto.pt>
2023-10-08 20:41:55 +0100

    Adaptação ao Template

--- include/applicationlayer.h
+++ include/applicationlayer.h
@@ -1,5 +1,9 @@
+#ifndef _APPLICATION_LAYER_H_
+#define _APPLICATION_LAYER_H_
 
 #include <stdio.h>
 
 
-void setupTransferConditions(char *port, int baudrate, const char role, unsigned int numTransmissions, unsigned int timeout, const char *filename);
+void setupTransferConditions(char *port, int baudrate, const char *role, unsigned int numTransmissions, unsigned int timeout, const char *filename);
+
+#endif
